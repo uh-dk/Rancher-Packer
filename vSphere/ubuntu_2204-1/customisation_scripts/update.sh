57aa25a1b6279d58a5b9b64368444bc42c026160
uh-dk <uhuber@dokumenta.de>
2022-06-18 22:00:13 +0200

    Update update.sh

--- vSphere/ubuntu_2204-1/customisation_scripts/update.sh
+++ vSphere/ubuntu_2204-1/customisation_scripts/update.sh
@@ -30,7 +30,7 @@ echo "update the package list"
 apt-get -y update;
 
 echo "install useful tools"
-apt-get install mc original-awk traceroute
+apt-get install mc original-awk traceroute net-tools
 
 echo "upgrade all installed packages incl. kernel and kernel headers"
 apt-get -y dist-upgrade -o Dpkg::Options::="--force-confnew";
