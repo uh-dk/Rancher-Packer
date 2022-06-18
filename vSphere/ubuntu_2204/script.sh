4663bd345fd2d847d1a9f6fff973e16efcdfe08d
uh-dk <ulysses.huber@dokumenta.de>
2022-06-18 14:01:52 +0200

    Init

--- vSphere/ubuntu_2204/script.sh
+++ vSphere/ubuntu_2204/script.sh
@@ -1,16 +1,9 @@
-# Remove snap
-snap remove core18
-snap remove core20
-snap remove lxd
-snap remove snapd
 # Apply updates and cleanup Apt cache
-# packer build --var-file=variables.json ubuntu-2204.json
-#apt-get install docker.io -y
+# packer build --var-file=variables.json ubuntu-2004.json
 apt-get update ; apt-get -y dist-upgrade
 apt-get -y autoremove
 apt-get -y clean
-apt-get purge --auto-remove multipath-tools snapd -y
-apt-get install mc original-awk
+apt-get install docker.io -y
 
 # Disable swap - generally recommended for K8s, but otherwise enable it for other workloads
 echo "Disabling Swap"
