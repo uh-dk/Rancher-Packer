1199739d106402211c8236a2e39e97d007c19494
uh-dk <uhuber@dokumenta.de>
2022-06-20 19:06:12 +0200

    Update networking.sh

--- vSphere/ubuntu_2204-1/customisation_scripts/networking.sh
+++ vSphere/ubuntu_2204-1/customisation_scripts/networking.sh
@@ -23,6 +23,6 @@ echo "apply netplan config"
 netplan generate && netplan apply
 
 # Disable Predictable Network Interface names and use eth0
-[ -e /etc/network/interfaces ] && sed -i 's/en[[:alnum:]]*/eth0/g' /etc/network/interfaces;
-sed -i 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0 \1"/g' /etc/default/grub;
-update-grub;
+#[ -e /etc/network/interfaces ] && sed -i 's/en[[:alnum:]]*/eth0/g' /etc/network/interfaces;
+#sed -i 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0 \1"/g' /etc/default/grub;
+#update-grub;
