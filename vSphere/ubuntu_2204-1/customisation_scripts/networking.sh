c38275fbcd095ba5f75dda677d69a8afac95febd
uh-dk <uhuber@dokumenta.de>
2022-06-20 21:03:38 +0200

    Update networking.sh

--- vSphere/ubuntu_2204-1/customisation_scripts/networking.sh
+++ vSphere/ubuntu_2204-1/customisation_scripts/networking.sh
@@ -20,6 +20,6 @@ else
 fi
 
 # Disable Predictable Network Interface names and use eth0
-#[ -e /etc/network/interfaces ] && sed -i 's/en[[:alnum:]]*/eth0/g' /etc/network/interfaces;
-#sed -i 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0 \1"/g' /etc/default/grub;
-#update-grub;
+[ -e /etc/network/interfaces ] && sed -i 's/en[[:alnum:]]*/eth0/g' /etc/network/interfaces;
+sed -i 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0 \1"/g' /etc/default/grub;
+update-grub;
